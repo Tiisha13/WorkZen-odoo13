3e39c84988089134d702fc06d0506b29d594a184
Shani-Sinojiya <72117665+Shani-Sinojiya@users.noreply.github.com>
2025-11-08 11:29:37 +0000

    Sync backend: feat: Add SMTP configuration constants and update server constants for frontend URL

--- backend/constants/server.go
+++ backend/constants/server.go
@@ -4,6 +4,7 @@ import "api.workzen.odoo/config"
 
 var (
 	// server
-	ServerPort = config.GetConfig().GetString("server.port")
-	ServerMode = config.GetConfig().GetString("server.mode")
+	ServerPort  = config.GetConfig().GetString("server.port")
+	ServerMode  = config.GetConfig().GetString("server.mode")
+	FrontendURL = config.GetConfig().GetString("frontend_url")
 )
